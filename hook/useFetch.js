929fea8b20a7b9edceccbe85a55acfe5a8d4deb3
Nuriddin-775 <alijonovnuriddin271@gmail.com>
2023-10-28 12:40:32 +0500

    popular jobs complete

---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useFetch(endpoint, query) {
+  const [data, setData] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "6c0b372266msh6b19b28a4e0a771p1c9137jsnaec5327c53f3",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: {
+      ...query,
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isloading, error, refetch };
+}
+
+export default useFetch;
